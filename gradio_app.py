d36b66c61ab3e4ac71072fa8f027ee75e430aa63
Onuralp SEZER <thunderbirdtr@gmail.com>
2024-09-23 06:44:16 +0300

    feat: 🚀 f1 metrics json and js results are generated and gradio app adjusted
    Signed-off-by: Onuralp SEZER <thunderbirdtr@gmail.com>

--- gradio_app.py
+++ gradio_app.py
@@ -1,24 +1,34 @@
 import json
+import sys
 from pathlib import Path
 from typing import Any, Dict, List
 
 import gradio as gr
 
+TITLE = """<h1 align="center">Model Leaderboard </h1>"""
+DESC = """
+<div style="text-align: center; display: flex; justify-content: center; align-items: center;">
+    <italic>powered by: &nbsp<a href='https://github.com/roboflow/supervision'>
+    <img src='https://supervision.roboflow.com/latest/assets/supervision-lenny.png'
+    height=24 width=24 style='display: inline-block'> supervision</a></italic>
+        <a href="https://github.com/roboflow/supervision">
+    <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/roboflow/supervision"
+        style="margin-right: 10px;">
+    </a>
+</div>
+"""  # noqa: E501 title/docs
+
 
 def load_results() -> List[Dict]:
-    results_list = []
-    for model_dir in Path("models/object_detection").iterdir():
-        if model_dir.is_file() or model_dir.name.startswith("_"):
-            continue
-        results_file = model_dir / "results.json"
-        if not results_file.exists():
-            print(f"Results file not found for {model_dir.name}")
-            continue
-        with open(results_file) as f:
-            results = json.load(f)
-            results_list.append(results)
-    results_list.sort(key=lambda x: x["metadata"]["model"])
-    return results_list
+    results: List[Dict] = []
+    results_file: Path = Path("static/aggregate_results.json")
+    if not results_file.exists():
+        print("aggregate_results.json file not found")
+        sys.exit(1)
+    with open(results_file) as f:
+        results = json.load(f)
+    results.sort(key=lambda x: x["metadata"]["model"])
+    return results
 
 
 def get_result_header() -> List[str]:
@@ -31,6 +41,14 @@ def get_result_header() -> List[str]:
         "mAP 50:95 (Small)",
         "mAP 50:95 (Medium)",
         "mAP 50:95 (Large)",
+        "F1 50",
+        "F1 75",
+        "F1 50 (Small)",
+        "F1 75 (Small)",
+        "F1 50 (Medium)",
+        "F1 75 (Medium)",
+        "F1 50 (Large)",
+        "F1 75 (Large)",
     ]
 
 
@@ -50,6 +68,14 @@ def parse_result(result: Dict) -> List[Any]:
         round(result["small_objects"]["map50_95"], round_digits),
         round(result["medium_objects"]["map50_95"], round_digits),
         round(result["large_objects"]["map50_95"], round_digits),
+        round(result["f1_50"], round_digits),
+        round(result["f1_75"], round_digits),
+        round(result["f1_small_objects"]["f1_50"], round_digits),
+        round(result["f1_small_objects"]["f1_75"], round_digits),
+        round(result["f1_medium_objects"]["f1_50"], round_digits),
+        round(result["f1_medium_objects"]["f1_75"], round_digits),
+        round(result["f1_large_objects"]["f1_50"], round_digits),
+        round(result["f1_large_objects"]["f1_75"], round_digits),
     ]
 
 
@@ -59,13 +85,8 @@ header = get_result_header()
 
 with gr.Blocks() as demo:
     gr.Markdown("# Model Leaderboard")
-    gr.HTML(
-        """
-        <italic>powered by: &nbsp<a href='https://github.com/roboflow/supervision'>
-        <img src='https://supervision.roboflow.com/latest/assets/supervision-lenny.png'
-        height=24 width=24 style='display: inline-block'> supervision</a></italic>
-        """
-    )
+    gr.HTML(TITLE)
+    gr.HTML(DESC)
     gr.DataFrame(headers=header, value=results)
 
 demo.launch()
