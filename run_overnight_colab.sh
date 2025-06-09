e957edddf18acd853d9764de108ed79f7b7f9e7c
AlexBodner <yodabodner@gmail.com>
2025-06-09 12:44:55 -0300

    update colab run

--- run_overnight_colab.sh
+++ run_overnight_colab.sh
@@ -33,6 +33,7 @@ for folder in ${folders[@]}; do
 
         # Override supervision version
         $VENV_PIP install --force-reinstall --no-deps "git+https://github.com/rafaelpadilla/supervision.git@fix/mAP"
+        export MPLBACKEND=Agg
 
         # Run script
         $VENV_PY run.py
