7723f832c8663d20b59b8ecef6c17157d56b7fd7
AlexBodner <yodabodner@gmail.com>
2025-06-09 11:58:04 -0300

    change to run overnight script

--- run_overnight.sh
+++ run_overnight.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
 
-# This script is made to be run over night, producing results.py for every model in the object_detection folder.
-# I hacked it together for initial launch only, but it might be useful as a starting point for later automated solutions.
+# This script is made to be run overnight, producing results.json for every model in the object_detection folder.
 
 current_path=$(pwd)
 
-# Iteartion order. Start with quick-running ones to surface errors faster.
+# Iteration order. Start with quick-running ones to surface errors faster.
 folders=(
     "yolov8"
     "yolov9"
@@ -14,22 +13,23 @@ folders=(
     "rt-detr"
 )
 
-
 for folder in ${folders[@]}; do
-    cd $current_path/models/object_detection/$folder
+    cd "$current_path/models/object_detection/$folder"
+    
     # If results.json exists, move it to results.json.old
     if [ -f results.json ]; then
         mv results.json results.json.old
     fi
 
-    # If folder stards with yolov9, use special rules
+    # If results.json doesn't exist, set up and run
     if [ ! -f results.json ]; then
-        if [[ $folder == yolov9* ]]; then
-            bash run_predictions.sh
-        fi
         uv venv
         source .venv/bin/activate
         uv pip install -r requirements.txt
+        
+        # Force install custom supervision version
+        pip install --force-reinstall --no-deps "git+https://github.com/rafaelpadilla/supervision.git@fix/mAP"
+        
         python run.py
         deactivate
     else
