dd1eacf1f5093953366ca91bb30ec7aa3b4a207f
AlexBodner <yodabodner@gmail.com>
2025-06-11 17:18:58 -0300

    rt-detr with each one in a different process

--- models/object_detection/rt-detr/run.py
+++ models/object_detection/rt-detr/run.py
@@ -11,6 +11,7 @@ from supervision.metrics import F1Score, MeanAveragePrecision
 from tqdm import tqdm
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
+import multiprocessing
 
 from configs import CONFIDENCE_THRESHOLD, DATASET_DIR
 from utils import (
@@ -46,8 +47,65 @@ TRANSFORMS = T.Compose(
     [T.Resize((RUN_PARAMETERS["imgsz"], RUN_PARAMETERS["imgsz"])), T.ToTensor()]
 )
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def run_single_model(
+    model_id: str,
+    skip_if_result_exists=False,
+    dataset: Optional[sv.DetectionDataset] = None,
+    ) -> None :
+    model_values = MODEL_DICT[model_id]
 
+    if skip_if_result_exists and result_json_already_exists(model_id):
+        print(f"Skipping {model_id}. Result already exists!")
+        return 
+    if dataset is None:
+        dataset = load_detections_dataset(DATASET_DIR)
 
+    model = torch.hub.load(HUB_URL, model_values["hub_id"], pretrained=True)
+    model = model.to(DEVICE)
+
+    predictions = []
+    targets = []
+    print("Evaluating...")
+    for _, image, target_detections in tqdm(dataset, total=len(dataset)):
+        img = Image.fromarray(image)
+        width, height = img.size
+        orig_size = torch.tensor([width, height])[None].to(DEVICE)
+        im_data = TRANSFORMS(img)[None].to(DEVICE)
+
+        results = model(im_data, orig_size)
+        labels, boxes, scores = results
+        class_id = labels.detach().cpu().numpy().astype(int)
+        xyxy = boxes.detach().cpu().numpy()
+        confidence = scores.detach().cpu().numpy()
+
+        detections = sv.Detections(
+            xyxy=xyxy[0],
+            confidence=confidence[0],
+            class_id=class_id[0],
+        )
+
+        detections = detections[detections.confidence > CONFIDENCE_THRESHOLD]
+        predictions.append(detections)
+
+        target_detections.mask = None
+        targets.append(target_detections)
+
+    mAP_metric = MeanAveragePrecision()
+    f1_metric = F1Score()
+    f1_result = f1_metric.update(predictions, targets).compute()
+    mAP_result = mAP_metric.update(predictions, targets).compute()
+
+    write_result_json(
+        model_id=model_id,
+        model_name=model_values["name"],
+        model_git_url=GIT_REPO_URL,
+        paper_url=PAPER_URL,
+        model=model,
+        mAP_result=mAP_result,
+        f1_score_result=f1_result,
+        license_name=LICENSE,
+        run_parameters=RUN_PARAMETERS,
+    )
 def run(
     model_ids: List[str],
     skip_if_result_exists=False,
@@ -66,61 +124,9 @@ def run(
 
     for model_id in model_ids:
         print(f"\nEvaluating model: {model_id}")
-        model_values = MODEL_DICT[model_id]
-
-        if skip_if_result_exists and result_json_already_exists(model_id):
-            print(f"Skipping {model_id}. Result already exists!")
-            continue
-
-        if dataset is None:
-            dataset = load_detections_dataset(DATASET_DIR)
-
-        model = torch.hub.load(HUB_URL, model_values["hub_id"], pretrained=True)
-        model = model.to(DEVICE)
-
-        predictions = []
-        targets = []
-        print("Evaluating...")
-        for _, image, target_detections in tqdm(dataset, total=len(dataset)):
-            img = Image.fromarray(image)
-            width, height = img.size
-            orig_size = torch.tensor([width, height])[None].to(DEVICE)
-            im_data = TRANSFORMS(img)[None].to(DEVICE)
-
-            results = model(im_data, orig_size)
-            labels, boxes, scores = results
-            class_id = labels.detach().cpu().numpy().astype(int)
-            xyxy = boxes.detach().cpu().numpy()
-            confidence = scores.detach().cpu().numpy()
-
-            detections = sv.Detections(
-                xyxy=xyxy[0],
-                confidence=confidence[0],
-                class_id=class_id[0],
-            )
-
-            detections = detections[detections.confidence > CONFIDENCE_THRESHOLD]
-            predictions.append(detections)
-
-            target_detections.mask = None
-            targets.append(target_detections)
-
-        mAP_metric = MeanAveragePrecision()
-        f1_metric = F1Score()
-        f1_result = f1_metric.update(predictions, targets).compute()
-        mAP_result = mAP_metric.update(predictions, targets).compute()
-
-        write_result_json(
-            model_id=model_id,
-            model_name=model_values["name"],
-            model_git_url=GIT_REPO_URL,
-            paper_url=PAPER_URL,
-            model=model,
-            mAP_result=mAP_result,
-            f1_score_result=f1_result,
-            license_name=LICENSE,
-            run_parameters=RUN_PARAMETERS,
-        )
+        process = multiprocessing.Process(target=run_single_model, args=(model_ids, skip_if_result_exists,dataset))
+        process.start()
+        process.join()
 
 
 if __name__ == "__main__":
