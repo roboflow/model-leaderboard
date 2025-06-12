f8cfb75ef2cd6481ffe1058ed0521fc6619387f5
AlexBodner <yodabodner@gmail.com>
2025-06-11 22:08:28 -0300

    fixed syntax err

--- run_overnight_colab.sh
+++ run_overnight_colab.sh
@@ -44,7 +44,7 @@ for folder in ${folders[@]}; do
         # Run script
         $VENV_PY run.py
         # Copy only .json files to Drive
-            find "$current_path/models/object_detection/$folder" -name "*.json" -exec cp --parents {} /content/drive/MyDrive/ \;
+        find "$current_path/models/object_detection/$folder" -name "*.json" -exec cp --parents {} /content/drive/MyDrive/ \;
         done    
     else
         echo "results.json already exists in $folder"
