4b12f8ac4a54db6714f454949d451759571f9949
jirka <6035284+Borda@users.noreply.github.com>
2026-01-14 16:50:35 +0100

    Refactor dataset and model paths using `pathlib` for improved flexibility and maintainability.

--- models/object_detection/configs.py
+++ models/object_detection/configs.py
@@ -1,2 +1,7 @@
+from pathlib import Path
+
 CONFIDENCE_THRESHOLD = 0
-DATASET_DIR = "../../../data/coco-val-2017"
+PATH_TO_ROOT = Path(__file__).resolve().parent.parent.parent
+PATH_TO_MODEL = PATH_TO_ROOT / "model"
+PATH_TO_DATA = PATH_TO_ROOT / "data"
+DATASET_DIR = str(PATH_TO_DATA  / "coco-val-2017")
